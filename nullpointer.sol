6f4236fd2c9fca6ce1e612188792828ec81abe04
mehak4563 <108951909+mehak4563@users.noreply.github.com>
2022-10-24 10:47:44 +0500

    Create nullpointer.sol

---
+++ nullpointer.sol
@@ -0,0 +1,16 @@
+#include <algorithm>
+#include <chrono>
+#include <iostream>
+#include<vector>
+using namespace std;
+using namespace std::chrono;
+
+int main()
+{
+
+    int a = 52;
+    int* ptr = NULL;
+    cout << ptr << endl;
+
+    return 0;
+}
